e87f9a78748d7f9f966592f3da8e1af98f55d4a8
Mihir <mwadekar2000@gmail.com>
2019-06-23 19:05:38 +0530

    Added code for Queue

--- Queue/Queue.go
+++ Queue/Queue.go
@@ -1,5 +1,43 @@
-package main
+package queue
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type Queue struct {
+	data list.List
+	size uint64
+}
+
+func (q *Queue) Size() uint64 {
+	return q.size
+}
+
+func (q *Queue) Empty() bool {
+	return q.size == 0
+}
+
+func (q *Queue) Push(value interface{}) {
+	q.data.PushBack(value)
+	q.size++
+}
+
+func (q *Queue) Top() interface{} {
+	return q.data.Front().Value
+}
+
+func (q *Queue) Pop() {
+	q.data.Remove(q.data.Front())
+	q.size--
+}
 
 func main() {
-	l := new(LinkedList)
+	q := new(Queue)
+	q.Push('a')
+	q.Push('b')
+	q.Push('c')
+	q.Pop()
+	q.Pop()
+	fmt.Printf("%c\n", q.Top())
 }
