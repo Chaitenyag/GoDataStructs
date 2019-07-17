4f0c747be99d63f78f6714fdade9127761032050
Chaitenya Gupta <48187468+Chaitenyag@users.noreply.github.com>
2019-07-17 10:53:28 +0400

    Add files via upload

--- HashTable/HashTable.go
+++ HashTable/HashTable.go
@@ -1,1 +1,29 @@
+package Hash
 
+import (
+	"fmt"
+	"hash"
+)
+
+const capacity = 15
+
+type Node struct {
+	key int
+	nextNode *Node
+}
+
+type Table struct {
+	ht map[int]*Node
+	curSize int
+}
+
+func Hash(key, ts int) int {
+	return key % ts
+}
+
+func Insert(hash *Table, key int) {
+	pos := Hash(key, hash.curSize)
+	element := Node{key: key, nextNode: hash.ht[pos]}
+	hash.ht[pos] = &element
+	hash.curSize++;
+}
